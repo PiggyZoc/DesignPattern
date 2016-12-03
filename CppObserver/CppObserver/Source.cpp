a82ee2b3f954e137ae19eae1aef2605550e58631
PiggyZoc <994280087@qq.com>
2016-12-03 18:55:59 +0800

    CppObserver 结束++

--- CppObserver/CppObserver/Source.cpp
+++ CppObserver/CppObserver/Source.cpp
@@ -11,140 +11,229 @@ class Observer
 {
 public:
 	Observer(){}
-	virtual ~Observer(){}
+	 ~Observer(){}
 	virtual void update(){};
 };
 
-class Ticket{
+class Observable
+{
 public:
-	Ticket(int num) :num_ticket(num){}
-	virtual ~Ticket(){}
-	void add_Buyer(Observer* obs){
-		this->List_Buyers.push_back(obs);
-	}
-
-	void add_Machine(Observer* obs){
-		this->Machine = obs;
-	}
-	void remove_obs(Observer* obs){
+	Observable(){}
+	~Observable(){}
+	list<Observer*> List_Obs;
+	bool addObserver(Observer* obs){
 		list<Observer*>::iterator ite;
-		for (ite = List_Buyers.begin(); ite != List_Buyers.end(); ite++)
+		for (ite = List_Obs.begin(); ite != List_Obs.end(); ite++)
 		{
 			if ((*ite) == obs){
-				ite = List_Buyers.erase(ite);
-				ite--;
-				break;
+				return false;
 			}
 		}
+		this->List_Obs.push_back(obs);
+		return true;
 	}
-	void NotifyToBuyers()
-	{
-		
-			list<Observer*>::iterator ite;
-			for (ite = List_Buyers.begin(); ite != List_Buyers.end();)
-			{
-
-				(*ite)->update();
-				List_Buyers.erase(ite++);
-				cout << endl;
-				
-			}
-
-		}
-	
-	void NotifyToMachine(){
-		if (this->GetNum() > 0)
-			return;
-		this->Machine->update();
-	}
-
-
-	int GetNum(){
-		return this->num_ticket;
-	}
-	void SetNum(int num){
-		this->num_ticket = num;
-	}
-	void AddATicket(){
-		this->num_ticket++;
-	}
-	void SellATicket(){
-		if (this->GetNum() > 0)
-			this->num_ticket--;
-		else
+	bool deleteObserver(Observer* obs){
+		list<Observer*>::iterator ite;
+		for (ite = List_Obs.begin(); ite != List_Obs.end(); ite++)
 		{
-			this->NotifyToMachine();
-			this->num_ticket--;
+			if ((*ite) == obs){
+				ite = List_Obs.erase(ite);
+				ite--;
+				return true;
+			}
 		}
+		return false;
 	}
+	virtual void Notify(){}
 
-private:
-	list<Observer*> List_Buyers;
-
-	
-	Observer* Machine;
-	int num_ticket;
 
 };
 
-class Buyer :public Observer{
-private:
-	string buyer_name;
-	Ticket* ticket;
-
-public:
-
-	Buyer(string m_name, Ticket* m_ticket) :buyer_name(m_name), ticket(m_ticket){}
-	~Buyer(){};
-	void BuyATicket(){
-
-			ticket->SellATicket();
-
-	}
-	void update(){
-		int get_ticket_num = ticket->GetNum();
-		cout << "<<<<<<  " << "当前余票为： " << get_ticket_num << "  >>>>>>"<<endl;
-		this->BuyATicket();
-		cout << this->buyer_name << "已获得  1  张票" << endl;
-	}
-	};
-class Machine :public Observer
+class NoticeBoard :public Observable
 {
-private:
-	string machine_id;
-	Ticket* ticket;
 public:
-	Machine(string m_id, Ticket* ticket) :machine_id(m_id), ticket(ticket){}
-	~Machine(){}
-	void update(){
-		cout << "机器正在为您取票，请耐心等待。。。\n" << endl;
-		Sleep(2000);
-		this->ticket->AddATicket();
-		cout << "机器已为您产生  1  张票" << endl;
+	NoticeBoard(){
+		contents[0] = "Undetermined";
+		contents[1] = "Have Ship!";
+		contents[2] = "No ship,sorry!";
+	}
+	~NoticeBoard(){}
+	void SetStatus(int i){
+		if (!(i == 1 || i == 2 || i == 3)) return;
+		this->current_status = i;
+		this->SetContent();
+	}
+	int GetStatus(){
+		return this->current_status;
+	}
+	void SetContent(){
+		int temp = this->current_status;
+		string temp_s;
+		if (temp == 1)
+			temp_s = this->contents[0];
+		if (temp == 2)
+	    	temp_s = this->contents[1];
+		if (temp == 3)
+			temp_s = this->contents[2];
+		this->current_content = temp_s;
+	}
+	string GetContent(){
+		return this->current_content;
+	}
+	bool hasChanged(){
+		if (this->previous_status != this->current_status){
+			this->previous_status = this->current_status;
+			return true;
+		}
+		else
+			return false;
+	}
+	void Notify(){
+		if (!hasChanged()) return;
+		list<Observer*>::iterator ite;
+		for (ite = List_Obs.begin(); ite != List_Obs.end(); ite++){
+			(*ite)->update();
+		}
 
 	}
-
+private:
+	int previous_status=-1;
+	int current_status=-1;
+	string current_content;
+	string contents[10];
 };
+class Passenger :public Observer
+{
+public:
+	Passenger(string name, NoticeBoard*noticeboard) :name(name), noticeboard(noticeboard){
+		moods[0] = "Anxious";
+		moods[1] = "Happy";
+		moods[2] = "Upset";
+		
+	}
+	~Passenger(){}
+	
+	string GetMood(){
+		return this->current_mood;
+	}
+	void update(){
+		int t = this->noticeboard->GetStatus();
+		this->SetMood(t);
+		cout<<this->name<<"--------------" << GetMood() << endl;
+	}
+private:
+	string name;
+	NoticeBoard* noticeboard;
+	string moods[10];
+	string current_mood;
+	void SetMood(int t){
+		int temp = t;
+		string temp_s;
+		if (temp == 1)
+			temp_s = this->moods[0];
+		if (temp == 2)
+			temp_s = this->moods[1];
+		if (temp == 3)
+			temp_s = this->moods[2];
 
+		this->current_mood = temp_s;
+	}
+};
+void Print(string s)
+{
+	cout << "##           Ship Schedule:           ##" << endl;
+	cout << endl;
+	cout << "##        From Shanghai To Taibei     ##" << endl;
+	cout << endl;
+	cout << "==========================================" << endl;
+	cout << endl;
+	cout << "           " << s << "                  " << endl;
+	cout << endl;
+	cout << "==========================================" << endl;
+	cout << endl;
+	cout << "Choose one of the following options to update status：" << endl;
+	cout << endl;
+	cout << "A. Undetermined  B.Have Ship  C.No Ship   E.Exit" << endl;
+	cout << endl;
+}
 int main()
 {
-	Buyer *Buyers[10];
+
+	NoticeBoard* noticeboard = new NoticeBoard();
+	Passenger* passenger1 = new Passenger("Wang Ming", noticeboard);
+	Passenger* passenger2 = new Passenger("Zhang Qiang", noticeboard);
+	Passenger* passenger3 = new Passenger("Li Xiang", noticeboard);
+	noticeboard->addObserver(passenger1);
+	noticeboard->addObserver(passenger2);
+	noticeboard->addObserver(passenger3);
+
+	noticeboard->deleteObserver(passenger2);
+
+	string s = "Welcome!";
+	Print(s);
+
+	//Graph<char,int> MIN;
+
+
 	
-	Ticket* sh_ticket = new Ticket(4);
-	for (int i = 0; i < 10; i++)
+
+	char in;
+	cout << "\nPlease choose your option：";
+	while (cin >> in)
 	{
-		string s = "Waiter";
-		Buyers[i] = new Buyer(s.append(to_string(i+1)), sh_ticket);
-		sh_ticket->add_Buyer(Buyers[i]);
+		if (in == 'E') {
+
+			cout << "\nThanks！！\n" << endl;
+			break;
+		}
+
+		switch (in)
+		{
+		case 'A':
+		{
+			system("cls");
+			noticeboard->SetStatus(1);
+			s = noticeboard->GetContent();
+			Print(s);
+			noticeboard->Notify();
+			break;
+
+		};
+		case 'B':
+		{
+			system("cls");
+			noticeboard->SetStatus(2);
+			s = noticeboard->GetContent();
+			Print(s);
+			noticeboard->Notify();
+			break;
+
+		};
+		case 'C':
+		{
+			system("cls");
+			noticeboard->SetStatus(3);
+			s = noticeboard->GetContent();
+			Print(s);
+			noticeboard->Notify();
+			break;
+
+		};
+		
+		default:
+		{
+			cout << "PLEASE INPUT AGAIN!!" << endl;
+			break;
+		};
+
+
+		}
+
+		Sleep(3000);
+		cout << "\nPlease choose your option：";
 	}
-//	sh_ticket->NotifyToBuyers();
 	
-	Machine machine_one("001", sh_ticket);
 
-	sh_ticket->add_Machine(&machine_one);
-	sh_ticket->NotifyToBuyers();
-
-	
 	system("PAUSE");
 	return 0;
 }
